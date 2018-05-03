baa718c7049e904d6a3ba3c6a649920e6637e6d6
wiselh <wise.self.2@gmail.com>
2018-05-03 17:15:17 +0100

    Set Login & Register

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { User } from '../../models/User';
+import { UserService } from '../../services/user.service';
+import { ToastController } from 'ionic-angular';
+import { RegisterPage } from '../register/register';
+import { HomePage } from '../home/home';
 
 @IonicPage()
 @Component({
@@ -15,11 +13,43 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  users: User[];
+  user: User = {
+    email: '',
+    password: ''
+  };
+  notLoggedIn: boolean = true;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, 
+    private userService: UserService, private toastCtrl: ToastController) {
+    this.userService.getUsers().subscribe(users => {
+      this.users = users;
+      
+    });
+  }
+  signIn(){
+    for (var key in this.users) {
+      var user = this.users[key];
+      if (user.email == this.user.email && user.password == this.user.password) {
+        this.presentToast('Logged in successefuly as ' + this.user.email);
+        this.navCtrl.setRoot(HomePage);
+        this.notLoggedIn = false;
+      }
+    }
+    if (this.notLoggedIn) {
+      this.presentToast('Oops! Your informations are not correct!');
+    }
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  signUp(){
+    this.navCtrl.push(RegisterPage);
   }
 
+  presentToast(message) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000
+    });
+    toast.present();
+  }
 }
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the RegisterPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { User } from '../../models/User';
+import { Observable } from 'rxjs/observable';
+import { LoginPage } from '../login/login';
 
 @IonicPage()
 @Component({
@@ -15,11 +12,39 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RegisterPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  usersCollection: AngularFirestoreCollection<User>;
+  users: Observable<User[]>;
+  userDoc: AngularFirestoreDocument<User>;
+
+  user: User = {
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+    createdAt: new Date().toLocaleDateString()
+  };
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private _db: AngularFirestore, private toastCtrl: ToastController) {
+    this.usersCollection = this._db.collection("users");
+  }
+  register() {
+    this.usersCollection.add(this.user)
+    .then(result => {
+      this.presentToast('Registred successefuly');
+      this.navCtrl.setRoot(LoginPage);
+    })
+    .catch(error =>{
+      this.presentToast(error);
+    });
+    
+  }
+  presentToast(message) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000
+    });
+    toast.present();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RegisterPage');
-  }
 
 }
